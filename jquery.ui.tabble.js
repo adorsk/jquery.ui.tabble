b8b032691d035f13845e6e01760b8bf7ed326d9c
adorsk <adorsk>
2012-12-12 08:45:57 -0500

    fixed sizing autodetect, less styling was interfering

--- jquery.ui.tabble.js
+++ jquery.ui.tabble.js
@@ -262,6 +262,7 @@
       else{
         targetWidth = fullWidth;
       }
+      console.log(delta, targetWidth, hWidth, bWidth);
 
       var cell_a_opts = {};
       cell_a_opts[dim] = targetWidth;
